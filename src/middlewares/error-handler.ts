30ae91ac9b305aee31556ebc0e667c643bf5fb2b
Arely <arely@fullstacklabs.co>
2023-06-23 16:59:04 -0600

    refactor: keep only primary errors

--- src/middlewares/error-handler.ts
+++ src/middlewares/error-handler.ts
@@ -1,75 +1,34 @@
-import { Request, Response } from 'express';
+import { Request, Response, NextFunction } from 'express';
 import { StatusCodes } from 'http-status-codes';
 import {
   ValidationError,
   NotFoundError,
-  DBError,
-  UniqueViolationError,
   NotNullViolationError,
-  ForeignKeyViolationError,
-  CheckViolationError,
-  DataError,
 } from 'objection';
 
-const errorHandler = (err: Error, _req: Request, res: Response) => {
+const errorHandler = (
+  err: Error,
+  _req: Request,
+  res: Response,
+  _next: NextFunction
+) => {
   let status = StatusCodes.BAD_REQUEST;
-  const message = err.message || 'Something went wrong';
+  let message = err.message;
   let type: string | undefined;
-  let data = {};
+  const data = {};
 
   if (err instanceof ValidationError) {
     status = StatusCodes.UNPROCESSABLE_ENTITY;
-    type = err.type;
-    switch (err.type) {
-      case 'ModelValidation':
-        data = err.data;
-        break;
-      case 'RelationExpression':
-        type = 'RelationExpression';
-        break;
-      default:
-        type = err.type || 'UnknownValidationError';
-        break;
-    }
+    type = err.type || 'ValidationError';
   } else if (err instanceof NotFoundError) {
     status = StatusCodes.NOT_FOUND;
     type = 'NotFound';
-  } else if (err instanceof UniqueViolationError) {
-    status = StatusCodes.CONFLICT;
-    type = 'UniqueViolation';
-    data = {
-      columns: err.columns,
-      table: err.table,
-      constraint: err.constraint,
-    };
   } else if (err instanceof NotNullViolationError) {
-    type = 'NotNullViolation';
-    data = {
-      column: err.column,
-      table: err.table,
-    };
-  } else if (err instanceof ForeignKeyViolationError) {
-    status = StatusCodes.CONFLICT;
-    type = 'ForeignKeyViolation';
-    data = {
-      table: err.table,
-      constraint: err.constraint,
-    };
-  } else if (err instanceof CheckViolationError) {
-    type = 'CheckViolation';
-    data = {
-      table: err.table,
-      constraint: err.constraint,
-    };
-  } else if (err instanceof DataError) {
-    type = 'InvalidData';
-  } else if (err instanceof DBError) {
-    status = StatusCodes.INTERNAL_SERVER_ERROR;
-    type = 'UnknownDatabaseError';
-  } else {
-    status = StatusCodes.INTERNAL_SERVER_ERROR;
-    type = 'UnknownError';
+    message = 'Missing required data';
+    type = err.name;
   }
+
+  type ||= 'UnknownError';
 
   return res.status(status).json({ message, type, data });
 };
