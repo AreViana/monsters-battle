6b4723d2385fb3d1e6e0d9725168838242751b4e
Arely <arely@fullstacklabs.co>
2023-06-23 17:02:07 -0600

    feat: add remove and create battle endpoints

--- src/controllers/__tests__/battle.spec.ts
+++ src/controllers/__tests__/battle.spec.ts
@@ -1,10 +1,10 @@
 import app from '../../app';
 import request from 'supertest';
 import { StatusCodes } from 'http-status-codes';
+import { Battle } from '../../models';
 
 const server = app.listen();
 
-beforeAll(() => jest.useFakeTimers());
 afterAll(() => server.close());
 
 describe('BattleController', () => {
@@ -18,29 +18,58 @@ describe('BattleController', () => {
 
   describe('Battle', () => {
     test('should fail when trying a battle of monsters with an undefined monster', async () => {
-      // @TODO
+      const response = await request(server)
+        .post('/battle')
+        .send({ monsterA: 1, monsterB: undefined });
+      expect(response.status).toBe(StatusCodes.UNPROCESSABLE_ENTITY);
+      expect(response.body.message).toBe('Missing monster id');
     });
 
     test('should fail when trying a battle of monsters with an inexistent monster', async () => {
-      // @TODO
+      const response = await request(server)
+        .post('/battle')
+        .send({ monsterA: 1, monsterB: 999 });
+      expect(response.status).toBe(StatusCodes.NOT_FOUND);
+      expect(response.body.message).toBe('Monster with id=999 not found');
+    });
+
+    test('should insert a battle of monsters successfully', async () => {
+      const response = await request(server)
+        .post('/battle')
+        .send({ monsterA: 3, monsterB: 4 });
+      expect(response.status).toBe(StatusCodes.CREATED);
     });
 
     test('should insert a battle of monsters successfully with monster 1 winning', async () => {
-      // @TODO
+      const response = await request(server)
+        .post('/battle')
+        .send({ monsterA: 3, monsterB: 2 });
+      expect(response.status).toBe(StatusCodes.CREATED);
+      expect(response.body.data.winner).toBe(3);
     });
 
     test('should insert a battle of monsters successfully with monster 2 winning', async () => {
-      // @TODO
+      const response = await request(server)
+        .post('/battle')
+        .send({ monsterA: 1, monsterB: 5 });
+      expect(response.status).toBe(StatusCodes.CREATED);
+      expect(response.body.data.winner).toBe(5);
     });
   });
 
   describe('Delete Battle', () => {
-    test('should delete a battle successfully', () => {
-      // @TODO
+    test('should delete a battle successfully', async () => {
+      const { id } = await Battle.query().insert({ monsterA: 1, monsterB: 2 });
+      const totalBattles = await Battle.query().resultSize();
+      const deleteResponse = await request(server).delete(`/battle/${id}`);
+      expect(await Battle.query().resultSize()).toBe(totalBattles - 1);
+      expect(deleteResponse.status).toBe(StatusCodes.NO_CONTENT);
     });
 
-    test("should return 404 if the battle doesn't exists", () => {
-      // @TODO
+    test("should return 404 if the battle doesn't exists", async () => {
+      const response = await request(server).delete(`/battle/9999`);
+      expect(response.status).toBe(StatusCodes.NOT_FOUND);
+      expect(response.body.message).toBe('Battle with id=9999 not found');
     });
   });
 });
--- src/controllers/battle.controller.ts
+++ src/controllers/battle.controller.ts
@@ -1,12 +1,32 @@
 import { Request, Response } from 'express';
 import { StatusCodes } from 'http-status-codes';
 import { Battle } from '../models';
+import { FigthService } from '../services/battle/figth.service';
+import { Id } from 'objection';
 
-const list = async (req: Request, res: Response): Promise<Response> => {
-  const battles = await Battle.query();
+const list = async (_req: Request, res: Response): Promise<Response> => {
+  const battles = await Battle.query().withGraphFetched(
+    '[monsterARelation, monsterBRelation, winnerRelation]'
+  );
   return res.status(StatusCodes.OK).json(battles);
+};
+
+const create = async (req: Request, res: Response): Promise<Response> => {
+  const { monsterA, monsterB } = req.body;
+  const battle = await FigthService.create(monsterA, monsterB);
+  return res.status(StatusCodes.CREATED).json({ data: battle });
+};
+
+const remove = async (req: Request, res: Response): Promise<Response> => {
+  const id: Id = req.params.id;
+  await Battle.query()
+    .deleteById(id)
+    .throwIfNotFound({ message: `Battle with id=${id} not found` });
+  return res.sendStatus(StatusCodes.NO_CONTENT);
 };
 
 export const BattleController = {
   list,
+  create,
+  remove,
 };
