bf6884fd8741b248ff911bed02ab37f245c53872
Arely <arely@fullstacklabs.co>
2023-06-23 15:57:10 -0600

    feat: solve problem 2

--- problem-solving/challenge-2/index.ts
+++ problem-solving/challenge-2/index.ts
@@ -3,5 +3,17 @@ export const diceFacesCalculator = (
   dice2: number,
   dice3: number
 ): number => {
-  return 0;
+  if (dice1 < 1 || dice1 > 6 || dice2 < 1 || dice2 > 6 || dice3 < 1 || dice3 > 6) {
+    throw new Error('Dice out of number range');
+  }
+
+  if (dice1 === dice2 && dice2 === dice3) {
+    return dice1 * 3;
+  } else if (dice1 === dice2 || dice1 === dice3) {
+    return dice1 * 2;
+  } else if (dice2 === dice3) {
+    return dice2 * 2;
+  }else {
+    return Math.max(dice1, dice2, dice3);
+  }
 };
