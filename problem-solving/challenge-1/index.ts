3f47447b9cc61c37e491ea00775aa37659c48a10
Arely <arely@fullstacklabs.co>
2023-06-23 15:56:57 -0600

    feat: solve problem 1

--- problem-solving/challenge-1/index.ts
+++ problem-solving/challenge-1/index.ts
@@ -1,1 +1,20 @@
-export const numbersFractionCalculator = (numbers: number[]) => {};
+export const numbersFractionCalculator = (numbers: number[]) => {
+  let positives = 0;
+  let negative = 0;
+  let zeros = 0;
+  numbers.forEach(num => {
+    if (num > 0) {
+      positives++;
+    } else if (num < 0) {
+      negative++;
+    } else if (num === 0){
+      zeros++;
+    }
+  });
+
+  return {
+      positives: (positives / numbers.length).toFixed(6),
+      negative: (negative / numbers.length).toFixed(6),
+      zeros: (zeros / numbers.length).toFixed(6),
+  };
+};
